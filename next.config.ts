738693befab6e26c5ec73070602b2c9c7f319ebe
nguyenhoanganh1808 <1808nguyenhoanganh2003@gmail.com>
2025-03-04 09:57:16 +0700

    fix: deploy error

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  output: "standalone",
+  output: "export",
   images: {
     domains: [
       "images.unsplash.com",
